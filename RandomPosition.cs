3cb1a5d394cf130d5080a05dcd7342ea6d6dd9d8
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-17 22:32:32 +0530

    Playerposition

---
+++ RandomPosition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class RandomPosition
+    {
+        public static void PlayerPosition()
+        {
+            
+            Random random = new Random();
+            int RollDice = random.Next(1, 6);
+            Console.WriteLine("Player rolls the dice and get: " + RollDice);
+        }
+    }
+}
