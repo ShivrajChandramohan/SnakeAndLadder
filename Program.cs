5fb562a15fbaedb304c1c88ebd63e6b4a22cc83b
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-17 21:52:46 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeLadder
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Snake and Ladder Problem");
+        }
+    }
+}
