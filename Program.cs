ae5ed148e42e668719ad7d084b22221db1bbded8
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-17 22:28:19 +0530

    Welcome

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace SnakeLadder
+﻿using SnakeAndLadderProblem;
+
+namespace SnakeLadder
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Snake and Ladder Problem");
+
+            //SinglePlayerPosition.PlayerStart();
+
+            //RandomPosition.PlayerPosition();
+
+            //RandomPositionCase.CasePosition();
+
+            //PlayerPostionTill100.ReachedHundred();
+
+            //PlayerExactPosition100.checkPosition();
+
+            PrintTotalNoOfDice.NumberOfDice();
         }
     }
 }
